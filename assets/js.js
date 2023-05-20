adb75c1f6a69ab2cbeaecc73bc97efc21263c163
gustsirt <gustavo.sirtori@gmail.com>
2023-05-19 21:34:40 -0300

    pagares

---
+++ assets/js.js
@@ -0,0 +1,289 @@
+const contenedor = document.getElementById('contenedor')
+// cantidades
+const nDesde = document.getElementById('pn-desde')
+const nHasta = document.getElementById('pn-hasta')
+const nTotal = document.getElementById('pn-tot')
+const nMonto = document.getElementById('pm-monto')
+let AuxHasta
+// fechas
+const fPagare = document.getElementById('pf-fecha')
+const fVencimiento = document.getElementById('pf-venc')
+// personas
+const pDestino = document.getElementById('p-destino')
+const pFirmante = document.getElementById('f-firmante')
+const pDNI = document.getElementById('f-dni')
+const pDireccion = document.getElementById('f-direccion')
+const pTel = document.getElementById('f-telefono')
+// notas
+const tIgualValor = document.getElementById('txt-igual-valor')
+const tPagaderoEn = document.getElementById('txt-pagadero-en')
+// boton
+const elBoton = document.getElementById('boton')
+
+// aux calculo fecha hoy
+let fechaHoyT
+function darDia (fecha) {
+    switch (fecha) {
+        case 1: return("lunes, "); break;
+        case 2: return("martes, "); break;
+        case 3: return("miércoles, "); break;
+        case 4: return("jueves, "); break;
+        case 5: return("viernes, "); break;
+        case 6: return("sábado, "); break;
+        case 7: return("domingo, "); break;
+        default: return(""); break;
+}}
+function darMes (fecha) {
+    switch (fecha) {
+        case 1: return("enero "); break;
+        case 2: return("febero "); break;
+        case 3: return("marzo "); break;
+        case 4: return("abril "); break;
+        case 5: return("mayo "); break;
+        case 6: return("junio "); break;
+        case 7: return("julio "); break;
+        case 8: return("agosto "); break;
+        case 9: return("septiembre "); break;
+        case 10: return("octubre "); break;
+        case 11: return("noviembre "); break;
+        case 12: return("diciembre "); break;
+        default: return(""); break;
+}}
+
+// conversor de numero a texto
+var numeroALetras = (function() {
+
+    // Código basado en https://gist.github.com/alfchee/e563340276f89b22042a
+        function Unidades(num){
+    
+            switch(num)
+            {
+                case 1: return 'UN';
+                case 2: return 'DOS';
+                case 3: return 'TRES';
+                case 4: return 'CUATRO';
+                case 5: return 'CINCO';
+                case 6: return 'SEIS';
+                case 7: return 'SIETE';
+                case 8: return 'OCHO';
+                case 9: return 'NUEVE';
+            }
+    
+            return '';
+        }//Unidades()
+    
+        function Decenas(num){
+    
+            let decena = Math.floor(num/10);
+            let unidad = num - (decena * 10);
+    
+            switch(decena)
+            {
+                case 1:
+                    switch(unidad)
+                    {
+                        case 0: return 'DIEZ';
+                        case 1: return 'ONCE';
+                        case 2: return 'DOCE';
+                        case 3: return 'TRECE';
+                        case 4: return 'CATORCE';
+                        case 5: return 'QUINCE';
+                        default: return 'DIECI' + Unidades(unidad);
+                    }
+                case 2:
+                    switch(unidad)
+                    {
+                        case 0: return 'VEINTE';
+                        default: return 'VEINTI' + Unidades(unidad);
+                    }
+                case 3: return DecenasY('TREINTA', unidad);
+                case 4: return DecenasY('CUARENTA', unidad);
+                case 5: return DecenasY('CINCUENTA', unidad);
+                case 6: return DecenasY('SESENTA', unidad);
+                case 7: return DecenasY('SETENTA', unidad);
+                case 8: return DecenasY('OCHENTA', unidad);
+                case 9: return DecenasY('NOVENTA', unidad);
+                case 0: return Unidades(unidad);
+            }
+        }//Unidades()
+    
+        function DecenasY(strSin, numUnidades) {
+            if (numUnidades > 0)
+                return strSin + ' Y ' + Unidades(numUnidades)
+    
+            return strSin;
+        }//DecenasY()
+    
+        function Centenas(num) {
+            let centenas = Math.floor(num / 100);
+            let decenas = num - (centenas * 100);
+    
+            switch(centenas)
+            {
+                case 1:
+                    if (decenas > 0)
+                        return 'CIENTO ' + Decenas(decenas);
+                    return 'CIEN';
+                case 2: return 'DOSCIENTOS ' + Decenas(decenas);
+                case 3: return 'TRESCIENTOS ' + Decenas(decenas);
+                case 4: return 'CUATROCIENTOS ' + Decenas(decenas);
+                case 5: return 'QUINIENTOS ' + Decenas(decenas);
+                case 6: return 'SEISCIENTOS ' + Decenas(decenas);
+                case 7: return 'SETECIENTOS ' + Decenas(decenas);
+                case 8: return 'OCHOCIENTOS ' + Decenas(decenas);
+                case 9: return 'NOVECIENTOS ' + Decenas(decenas);
+            }
+    
+            return Decenas(decenas);
+        }//Centenas()
+    
+        function Seccion(num, divisor, strSingular, strPlural) {
+            let cientos = Math.floor(num / divisor)
+            let resto = num - (cientos * divisor)
+    
+            let letras = '';
+    
+            if (cientos > 0)
+                if (cientos > 1)
+                    letras = Centenas(cientos) + ' ' + strPlural;
+                else
+                    letras = strSingular;
+    
+            if (resto > 0)
+                letras += '';
+    
+            return letras;
+        }//Seccion()
+    
+        function Miles(num) {
+            let divisor = 1000;
+            let cientos = Math.floor(num / divisor)
+            let resto = num - (cientos * divisor)
+    
+            let strMiles = Seccion(num, divisor, 'UN MIL', 'MIL');
+            let strCentenas = Centenas(resto);
+    
+            if(strMiles == '')
+                return strCentenas;
+    
+            return strMiles + ' ' + strCentenas;
+        }//Miles()
+    
+        function Millones(num) {
+            let divisor = 1000000;
+            let cientos = Math.floor(num / divisor)
+            let resto = num - (cientos * divisor)
+    
+            let strMillones = Seccion(num, divisor, 'UN MILLON DE', 'MILLONES DE');
+            let strMiles = Miles(resto);
+    
+            if(strMillones == '')
+                return strMiles;
+    
+            return strMillones + ' ' + strMiles;
+        }//Millones()
+    
+        return function NumeroALetras(num, currency) {
+            currency = currency || {};
+            let data = {
+                numero: num,
+                enteros: Math.floor(num),
+                centavos: (((Math.round(num * 100)) - (Math.floor(num) * 100))),
+                letrasCentavos: '',
+                letrasMonedaPlural: currency.plural || 'PESOS',//'PESOS', 'Dólares', 'Bolívares', 'etcs'
+                letrasMonedaSingular: currency.singular || 'PESO', //'PESO', 'Dólar', 'Bolivar', 'etc'
+                letrasMonedaCentavoPlural: currency.centPlural || 'CENTAVOS',
+                letrasMonedaCentavoSingular: currency.centSingular || 'CENTAVO'
+            };
+    
+            if (data.centavos > 0) {
+                data.letrasCentavos = 'CON ' + (function () {
+                        if (data.centavos == 1)
+                            return Millones(data.centavos) + ' ' + data.letrasMonedaCentavoSingular;
+                        else
+                            return Millones(data.centavos) + ' ' + data.letrasMonedaCentavoPlural;
+                    })();
+            };
+    
+            if(data.enteros == 0)
+                return 'CERO ' + data.letrasMonedaPlural + ' ' + data.letrasCentavos;
+            if (data.enteros == 1)
+                return Millones(data.enteros) + ' ' + data.letrasMonedaSingular + ' ' + data.letrasCentavos;
+            else
+                return Millones(data.enteros) + ' ' + data.letrasMonedaPlural + ' ' + data.letrasCentavos;
+        };
+    
+    })();
+
+// contenedor
+let contenedorPageres
+
+function Calculos() {
+    let contenedorPageres
+    // calculo fecha pagare
+    let fechaHoyN
+    if(fPagare.value==''){
+        fechaHoyN= new Date();
+    }else{
+        fechaHoyN=new Date(fPagare.value);
+        fechaHoyN.setDate(fechaHoyN.getDate()+1);
+    }
+    // calculo fecha pagare en formato extendido
+    let fechaHoyNT = fechaHoyN.toLocaleDateString('es-ar', { weekday:"long", year:"numeric", month:"long", day:"numeric"})
+
+    // calculo aux fecha vencimiento
+    let fVencimientoAux = new Date(fVencimiento.value);
+    fVencimientoAux.setDate(fVencimientoAux.getDate()+1);
+
+    // calculo HASTA
+    if (nHasta.value=='') {
+        AuxHasta = nTotal.value;
+    } else {
+        AuxHasta = nHasta.value;
+    }
+
+    // generacion de pagares
+    contenedor.innerHTML = ''
+    for (let i = nDesde.value-1; i < AuxHasta; i++) {
+        // calculo fecha venc
+        fVencimientoAux.setMonth(fVencimientoAux.getMonth()+1)
+
+
+        contenedorPageres = `<div class="modelo-caja"><div class="modelo-caja-interna">
+
+            <div  class="renglon-0">
+                <div class="renglon-1">
+                    <p>Pagare Nº ${i+1} de ${nTotal.value}</p>
+                    <p>Vence el ${fVencimientoAux.toLocaleDateString('es-ar', {year:"numeric", month:"numeric", day:"numeric"})}</p>
+                </div>
+                <p class="renglon-1-2"><strong>POR: </strong>${new Intl.NumberFormat('es-ar',{style: 'currency', currency: 'ARS'}).format(nMonto.value)}</p>
+            </div>
+
+            <div class="renglon-m">
+                <p>CORDOBA ${fechaHoyNT} <strong>PAGARÉ SIN PROTESTO</strong> (art. 50 D. Ley 5965/63) al señor ${pDestino.value} a su orden la cantidad de pesos argentino ${numeroALetras(nMonto.value)}</p>`
+
+        if ( tIgualValor.value!='' ) {
+            contenedorPageres +=`<p>Por Igual valor recibido en ${tIgualValor.value}</p>`}
+        if ( tPagaderoEn.value!='' ) {
+            contenedorPageres +=`<p>pagadero en ${tPagaderoEn.value}</p>`}
+
+        contenedorPageres +=`</div>
+            <div class="renglon-f">
+                <p>Firmante: ${pFirmante.value}</p>
+                <p>DNI: ${pDNI.value}</p>
+                <p>Dirección: ${pDireccion.value}</p>
+                <p>Telefono: ${pTel.value}</p>
+            </div>
+        </div></div><br>`
+
+        contenedor.innerHTML += contenedorPageres   
+    }
+}
+
+function iniciarCalculos() {
+
+    elBoton.addEventListener('click', Calculos)
+}
+
+
+window.addEventListener('load', iniciarCalculos)
